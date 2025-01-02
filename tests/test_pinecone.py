83d176209c9905776240383d5a158481330a6577
mahidhar <156211533+mahidhar1g@users.noreply.github.com>
2025-01-02 10:09:40 -0500

    Adding pincone testing file

--- tests/test_pinecone.py
+++ tests/test_pinecone.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 
-index_name = "quickstart"
+index_name = "firstindex"
 
 pc.create_index(
     name=index_name,
@@ -17,3 +17,19 @@ pc.create_index(
         region="us-east-1"
     ) 
 )
+
+data = [
+    {"id": "vec1", "text": "Apple is a popular fruit known for its sweetness and crisp texture."},
+    {"id": "vec2", "text": "The tech company Apple is known for its innovative products like the iPhone."},
+    {"id": "vec3", "text": "Many people enjoy eating apples as a healthy snack."},
+    {"id": "vec4", "text": "Apple Inc. has revolutionized the tech industry with its sleek designs and user-friendly interfaces."},
+    {"id": "vec5", "text": "An apple a day keeps the doctor away, as the saying goes."},
+    {"id": "vec6", "text": "Apple Computer Company was founded on April 1, 1976, by Steve Jobs, Steve Wozniak, and Ronald Wayne as a partnership."}
+]
+
+embeddings = pc.inference.embed(
+    model="multilingual-e5-large",
+    inputs=[d['text'] for d in data],
+    parameters={"input_type": "passage", "truncate": "END"}
+)
+print(embeddings[0])
